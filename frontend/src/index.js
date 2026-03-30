f0d9626edb334216e460bcb80102f01c063aff97
jiyavinchhi123 <jiya.vinchhi2412@gmail.com>
2026-03-30 09:16:01 +0530

    Delete frontend directory

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./styles.css";
-import "./styles1.css";
-
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
